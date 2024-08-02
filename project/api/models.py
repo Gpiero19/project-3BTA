2fe85118abd9b30892f94524479d1e8c318b2e5c
Gian Piero Canevari <canevarigian@gmail.com>
2024-08-02 20:40:14 +0200

    model correction
    related name was added
    + migration was made

--- project/api/models.py
+++ project/api/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Task(models.Model):
-    creator = models.ForeignKey(User, on_delete=models.CASCADE)
-    executor = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_tasks')
+    executor = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='assigned_tasks')
     name = models.CharField(max_length=255)
     cost = models.DecimalField(max_digits=8, decimal_places=2)
     is_done = models.BooleanField(default=False)
