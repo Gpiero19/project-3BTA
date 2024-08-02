9d40406c45c25057cdf824b4e215ff968c98512b
Gian Piero Canevari <canevarigian@gmail.com>
2024-08-02 19:21:24 +0200

    local url + global url
    correction of API/

--- project/api/urls.py
+++ project/api/urls.py
@@ -2,16 +2,16 @@ from django.urls import path
 
 
 urlpatterns = [
-    # path('app/user/create/', UserCreateView.as_view(), name='user_create'),
-    # path('app/login/', LoginView.as_view(), name='login'),
-    # path('app/logout/', LogoutView.as_view(), name='logout'),
-    # path('app/task/create/', TaskCreateView.as_view(), name='task_create'),
-    # path('app/tasks-created-by-user/', TasksCreatedByUser.as_view(), name='user_tasks'),
-    # path('app/user-tasks-stats/', UserTasksStatsAPIView.as_view(), name='user-tasks-stats'),
-    # path('app/unassigned-tasks/', UnassignedTasksAPIView.as_view(), name='unassigned-tasks'),
-    # path('app/task/executor/', TaskWithExecutorAPIView.as_view(), name='task_executor'),
-    # path('app/user-tasks/', UserTasksAPIView.as_view(), name='my-tasks'),
-    # path('app/become-executor/<int:task_id>/', BecomeExecutorAPIView.as_view(), name='become-executor'),
-    # path('app/mark-task-done/<int:task_id>/', MarkTaskDoneAPIView.as_view(), name='mark-task-done'),
-    # path('app/clear_db/', ClearDatabaseView.as_view(), name='clear_db'),
+    # path('user/create/', UserCreateView.as_view(), name='user_create'),
+    # path('login/', LoginView.as_view(), name='login'),
+    # path('logout/', LogoutView.as_view(), name='logout'),
+    # path('task/create/', TaskCreateView.as_view(), name='task_create'),
+    # path('tasks-created-by-user/', TasksCreatedByUser.as_view(), name='user_tasks'),
+    # path('user-tasks-stats/', UserTasksStatsAPIView.as_view(), name='user-tasks-stats'),
+    # path('unassigned-tasks/', UnassignedTasksAPIView.as_view(), name='unassigned-tasks'),
+    # path('task/executor/', TaskWithExecutorAPIView.as_view(), name='task_executor'),
+    # path('user-tasks/', UserTasksAPIView.as_view(), name='my-tasks'),
+    # path('become-executor/<int:task_id>/', BecomeExecutorAPIView.as_view(), name='become-executor'),
+    # path('mark-task-done/<int:task_id>/', MarkTaskDoneAPIView.as_view(), name='mark-task-done'),
+    # path('clear_db/', ClearDatabaseView.as_view(), name='clear_db'),
 ]
